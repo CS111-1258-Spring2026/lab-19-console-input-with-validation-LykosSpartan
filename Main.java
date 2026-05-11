fb5127c2a57a4bca912c82b0fa9b7c15dccc9778
Ricardo Rodriguez <lucario.the.hero@gmail.com>
2026-05-11 04:11:05 +0000

    Name, genre, rating

--- Main.java
+++ Main.java
@@ -1,9 +1,52 @@
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		
+		Scanner keyboard = new Scanner(System.in);
+		NetflixOriginal netflixOriginal1 = intstantiateFromImput(keyboard);
+		System.out.println("Netflix Original created: " + netflixOriginal1.getName() + ", " + netflixOriginal1.getStarRating() + " stars, Genre: " + netflixOriginal1.getGenre());
+		keyboard.close();
 
 	}
+public static NetflixOriginal intstantiateFromImput(Scanner keyboard){
+	String name, genre;
+	double starRating;
+
+	System.out.println("Enter the name of the Netflix Original: ");
+	name = keyboard.nextLine();
+	System.out.println("Enter the star rating of the Netflix Original (0.0 - 5	.0): ");
+	starRating = keyboard.nextDouble();
+	keyboard.nextLine(); //consume the newline character left by nextDouble()
+	System.out.println("Enter the genre of the Netflix Original: ");
+	genre = keyboard.nextLine();
+
+	//do while loop genre
+	while(!genre.equalsIgnoreCase("Action") && !genre.equalsIgnoreCase("Comedy") && !genre.equalsIgnoreCase("Drama") && !genre.equalsIgnoreCase("Horror") && !genre.equalsIgnoreCase("Sci-Fi"))
+	{
+		System.out.println("Invalid genre. Please enter one of the following genres: Action, Comedy, Drama, Horror, Sci-Fi: ");
+		genre = keyboard.nextLine();
+	}
+	
+	// Do while loop to get valid star rating from user input.
+	do
+	{
+		if(starRating < 0.0 || starRating > 5.0)
+		{
+			System.out.println("Invalid star rating. Please enter a value between 0.0 and 5.0: ");
+			starRating = keyboard.nextDouble();
+			keyboard.nextLine(); //consume the newline character left by nextDouble()
+		}
+	} while(starRating < 0.0 || starRating > 5.0);
+	
+	//create and intitialize a NetflixOriginal object with the user data.
+	NetflixOriginal tempNetflixOriginal = new NetflixOriginal(name, starRating, genre);
+
+	return tempNetflixOriginal;
+}
+
+
+	
 
 }
