3dada24f6c93c8f944e6c65b36c638c64d18698e
Ricardo Rodriguez <lucario.the.hero@gmail.com>
2026-05-11 21:28:48 +0000

    Do while loop change

--- Main.java
+++ Main.java
@@ -35,7 +35,13 @@ public static NetflixOriginal instantiateFromInput(Scanner keyboard){
 	System.out.print("Please enter the genre: ");
 	genre = keyboard.nextLine();
 
-	
+	do 
+	{
+		System.out.println("Invalid star rating. Please enter a value between 0.0 and 5.0: ");
+		starRating = keyboard.nextDouble();
+		keyboard.nextLine(); //consume the newline character left by nextDouble()
+	}while (starRating < 0.0 || starRating > 5.0);
+
 	
 	do
 	{
@@ -44,13 +50,7 @@ public static NetflixOriginal instantiateFromInput(Scanner keyboard){
 	} while(!genre.equalsIgnoreCase("drama") && !genre.equalsIgnoreCase("comedy") && !genre.equalsIgnoreCase("horror") && !genre.equalsIgnoreCase("action") && !genre.equalsIgnoreCase("science fiction") && !genre.equalsIgnoreCase("anime") && !genre.equalsIgnoreCase("reality") && !genre.equalsIgnoreCase("unknown"));
 	
 	// Do while loop to get valid star rating from user input.
-	while (starRating < 0.0 || starRating > 5.0)
-	{
-		System.out.println("Invalid star rating. Please enter a value between 0.0 and 5.0: ");
-		starRating = keyboard.nextDouble();
-		keyboard.nextLine(); //consume the newline character left by nextDouble()
-	}
-
+	
 
 	
 	//create and intitialize a NetflixOriginal object with the user data.
